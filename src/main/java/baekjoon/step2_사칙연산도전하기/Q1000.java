5e817b912e2463edaf0e6dcf4cb4f3956b774a76
moving-line <movinglinecheck@gmail.com>
2019-03-26 07:25:01 +0900

    Q1000 A + B

---
+++ src/main/java/baekjoon/step2_사칙연산도전하기/Q1000.java
@@ -0,0 +1,26 @@
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A+B를 출력한다.
+ */
+
+package baekjoon.step2_사칙연산도전하기;
+
+import java.util.Scanner;
+
+public class Q1000 {
+    public static void main(String[] args) {
+        int A, B;
+
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println(A + B);
+    }
+}
