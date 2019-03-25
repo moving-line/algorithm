2e36c317473214fa8f4488fbdb4af88686344405
moving-line <movinglinecheck@gmail.com>
2019-03-26 08:01:16 +0900

    Q2258 A + B - 2

---
+++ src/main/java/baekjoon/step2_사칙연산도전하기/Q2258.java
@@ -0,0 +1,24 @@
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A, 둘째 줄에 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A+B를 출력한다.
+ */
+
+package baekjoon.step2_사칙연산도전하기;
+
+import java.util.Scanner;
+
+public class Q2258 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + B);
+    }
+}
