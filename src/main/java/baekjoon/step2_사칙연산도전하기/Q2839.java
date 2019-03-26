ffc1e753bd31de1365151566fd960ccc06027d4a
moving-line <movinglinecheck@gmail.com>
2019-03-26 09:19:21 +0900

    Q2839 설탕 배달

---
+++ src/main/java/baekjoon/step2_사칙연산도전하기/Q2839.java
@@ -0,0 +1,63 @@
+/*
+문제
+상근이는 요즘 설탕공장에서 설탕을 배달하고 있다. 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다.
+설탕공장에서 만드는 설탕은 봉지에 담겨져 있다. 봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
+상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다.
+
+예를 들어, 18킬로그램 설탕을 배달해야 할 때, 3킬로그램 봉지 6개를 가져가도 되지만,
+5킬로그램 3개와 3킬로그램 1개를 배달하면, 더 적은 개수의 봉지를 배달할 수 있다.
+
+상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N이 주어진다. (3 ≤ N ≤ 5000)
+
+출력
+상근이가 배달하는 봉지의 최소 개수를 출력한다. 만약, 정확하게 N킬로그램을 만들 수 없다면 -1을 출력한다.
+ */
+
+package baekjoon.step2_사칙연산도전하기;
+
+import java.util.Scanner;
+
+public class Q2839 {
+    public static void main(String[] args) {
+        // 첫번째 풀이
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        while (N < 3 || N > 5000) N = sc.nextInt();
+
+        int answer = 0;
+
+        outer : for (int i = 0; i <= N; i++) {
+            boolean exist = false;
+            for (int j = 0; j <= N; j++) {
+                if ((3 * i) + (5 * j) == N) {
+                    answer = i + j;
+                    break outer;
+                }
+            }
+        }
+        if (answer == 0) answer = -1;
+
+        System.out.println(answer);
+
+//        두번째 풀이
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        while (N < 3 || N > 5000) N = sc.nextInt();
+//
+//        int count = 0;
+//        while(true) {
+//            if (N % 5 == 0) {
+//                System.out.println(N / 5 + count);
+//                return;
+//            } else if (N <= 0) {
+//                System.out.println(-1);
+//                return;
+//            }
+//            N -= 3;
+//            count++;
+//        }
+    }
+}
