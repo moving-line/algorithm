3029abf7e0c7c821ea4c3f81c6db83042d320ecb
moving-line <movinglinecheck@gmail.com>
2019-05-09 15:34:49 +0900

    Q1934 최소공배수

---
+++ src/main/java/baekjoon/step18_최대공약수와최소공배수/Q1934.java
@@ -0,0 +1,54 @@
+/*
+문제
+두 자연수 A와 B에 대해서, A의 배수이면서 B의 배수인 자연수를 A와 B의 공배수라고 한다. 이런 공배수 중에서 가장 작은 수를 최소공배수라고 한다.
+예를 들어, 6과 15의 공배수는 30, 60, 90등이 있으며, 최소 공배수는 30이다.
+
+두 자연수 A와 B가 주어졌을 때, A와 B의 최소공배수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 둘째 줄부터 T개의 줄에 걸쳐서 A와 B가 주어진다. (1 ≤ A, B ≤ 45,000)
+
+출력
+첫째 줄부터 T개의 줄에 A와 B의 최소공배수를 입력받은 순서대로 한 줄에 하나씩 출력한다.
+ */
+
+package baekjoon.step18_최대공약수와최소공배수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String[] input = br.readLine().split(" ");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+
+            System.out.println(lcm(A, B));
+        }
+    }
+
+    public static int lcm(int a, int b) {
+        int number1 = a;
+        int number2 = b;
+        int remain = 1;
+
+        if (b > a){
+            number1 = b;
+            number2 = a;
+        }
+
+        // 유클리드 호제법
+        while (remain > 0) {
+            remain = number1 % number2;
+            number1 = number2;
+            number2 = remain;
+        }
+
+        return a * b / number1;
+    }
+}
