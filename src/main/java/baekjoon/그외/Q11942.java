8f8b070febae437bbde4d62ab5cde0ce33e5f58f
moving-line <movinglinecheck@gmail.com>
2019-06-17 13:50:29 +0900

    Q11942 고려대는 사랑입니다

---
+++ src/main/java/baekjoon/그외/Q11942.java
@@ -0,0 +1,15 @@
+/*
+문제
+고려대학교는 사랑입니다.
+
+출력
+첫째 줄에 “고려대학교”를 출력하세요.
+ */
+
+package baekjoon.그외;
+
+public class Q11942 {
+    public static void main(String[] args) {
+        System.out.println("고려대학교");
+    }
+}
