25466960a2cb14bdefdfa9311668035d44e274f7
moving-line <movinglinecheck@gmail.com>
2019-07-06 13:02:36 +0900

    Q11021 A + B - 7

---
+++ src/main/java/baekjoon/step2_사칙연산도전하기/Q11021.java
@@ -0,0 +1,29 @@
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+ */
+
+package baekjoon.step2_사칙연산도전하기;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q11021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+        for (int i = 1; i <= T; i++) {
+            int[] input = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            System.out.printf("Case #%d: %d\n", i, input[0] + input[1]);
+        }
+    }
+}
