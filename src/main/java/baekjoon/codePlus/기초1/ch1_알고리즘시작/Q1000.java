433acdbf3c9293e17d21674549d71e384d29206b
moving-line <movinglinecheck@gmail.com>
2019-08-12 10:35:38 +0900

    Q1000 A+B

---
+++ src/main/java/baekjoon/codePlus/기초1/ch1_알고리즘시작/Q1000.java
@@ -0,0 +1,14 @@
+package baekjoon.codePlus.기초1.ch1_알고리즘시작;
+
+import java.util.Scanner;
+
+public class Q1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+    }
+}
