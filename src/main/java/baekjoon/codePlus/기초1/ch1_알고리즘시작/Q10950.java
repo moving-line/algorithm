9f902407ec2857d4e0624f720f17e5a70bc23314
moving-line <movinglinecheck@gmail.com>
2019-08-12 10:42:13 +0900

    Q10950 A+B - 3

---
+++ src/main/java/baekjoon/codePlus/기초1/ch1_알고리즘시작/Q10950.java
@@ -0,0 +1,15 @@
+package baekjoon.codePlus.기초1.ch1_알고리즘시작;
+
+import java.util.Scanner;
+
+public class Q10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            System.out.println(sc.nextInt() + sc.nextInt());
+        }
+    }
+}
