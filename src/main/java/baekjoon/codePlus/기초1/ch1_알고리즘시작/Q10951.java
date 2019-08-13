07d75dfacaeb4ac281deb4db96dbbde539de8de7
moving-line <movinglinecheck@gmail.com>
2019-08-13 13:54:39 +0900

    Q10951 A+B - 4

---
+++ src/main/java/baekjoon/codePlus/기초1/ch1_알고리즘시작/Q10951.java
@@ -0,0 +1,13 @@
+package baekjoon.codePlus.기초1.ch1_알고리즘시작;
+
+import java.util.Scanner;
+
+public class Q10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextInt()) {
+            System.out.println(sc.nextInt() + sc.nextInt());
+        }
+    }
+}
