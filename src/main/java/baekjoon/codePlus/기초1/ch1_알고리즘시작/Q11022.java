cfb82d961ad89b4c4a170dde9e67df7dc58fdc8f
moving-line <movinglinecheck@gmail.com>
2019-08-14 14:55:20 +0900

    Q11022 A+B - 8
    - Q11021의 오류도 수정

--- src/main/java/baekjoon/codePlus/기초1/ch1_알고리즘시작/Q11022.java
+++ src/main/java/baekjoon/codePlus/기초1/ch1_알고리즘시작/Q11022.java
@@ -2,7 +2,7 @@ package baekjoon.codePlus.기초1.ch1_알고리즘시작;
 
 import java.util.Scanner;
 
-public class Q11021 {
+public class Q11022 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -13,7 +13,7 @@ public class Q11021 {
             a = sc.nextInt();
             b = sc.nextInt();
 
-            System.out.println("Case #" + (i + 1) + ": " + (a + b));
+            System.out.printf("Case #%d: %d + %d = %d\n", (i + 1), a, b, (a + b));
         }
     }
 }
