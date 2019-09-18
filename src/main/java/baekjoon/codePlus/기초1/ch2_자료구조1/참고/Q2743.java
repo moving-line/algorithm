ca26eae56991929960f36d05094aa923ebc58c30
moving-line <movinglinecheck@gmail.com>
2019-09-18 10:36:57 +0900

    Q2743 단어 길이 재기

---
+++ src/main/java/baekjoon/codePlus/기초1/ch2_자료구조1/참고/Q2743.java
@@ -0,0 +1,13 @@
+package baekjoon.codePlus.기초1.ch2_자료구조1.참고;
+
+import java.util.Scanner;
+
+public class Q2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        System.out.println(input.length());
+    }
+}
