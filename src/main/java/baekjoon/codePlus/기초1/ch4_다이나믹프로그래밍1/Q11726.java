e950311aefb24f3331e2f66a60d835371bcde268
moving-line <movinglinecheck@gmail.com>
2019-09-27 15:00:42 +0900

    Q11726 2xn 타일링

---
+++ src/main/java/baekjoon/codePlus/기초1/ch4_다이나믹프로그래밍1/Q11726.java
@@ -0,0 +1,42 @@
+package baekjoon.codePlus.기초1.ch4_다이나믹프로그래밍1;
+
+import java.util.Scanner;
+
+public class Q11726 {
+    // Top-down
+    private static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dp = new int[n + 1];
+
+        System.out.println(calculate(n));
+    }
+
+    private static int calculate(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        }
+        if (dp[num] > 0) {
+            return dp[num];
+        }
+
+        dp[num] = (calculate(num - 1) + calculate(num - 2)) % 10007;
+        return dp[num];
+    }
+
+    // Bottom-up
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        int[] dp = new int[n + 1];
+//        dp[0] = dp[1] = 1;
+//
+//        for (int i = 2; i <= n; i++) {
+//            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+//        }
+//        System.out.println(dp[n]);
+//    }
+}
