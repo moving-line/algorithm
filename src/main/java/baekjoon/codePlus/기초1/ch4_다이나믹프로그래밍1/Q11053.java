88ff71eb2ac3b57faa20c91433ccb192cbe131ee
moving-line <movinglinecheck@gmail.com>
2019-10-05 12:22:57 +0900

    Q11053 가장 긴 증가하는 부분 수열

---
+++ src/main/java/baekjoon/codePlus/기초1/ch4_다이나믹프로그래밍1/Q11053.java
@@ -0,0 +1,28 @@
+package baekjoon.codePlus.기초1.ch4_다이나믹프로그래밍1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q11053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if(A[j] < A[i] && dp[j] + 1 < dp[i]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
