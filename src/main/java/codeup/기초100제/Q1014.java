e43f76e599019cbb1c5f8edefa78873ae6b465da
moving-line <movinglinecheck@gmail.com>
2019-10-10 15:41:50 +0900

    Q1014 ~ Q1015

---
+++ src/main/java/codeup/기초100제/Q1014.java
@@ -0,0 +1,13 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.printf("%s %s", b, a);
+    }
+}
---
+++ src/main/java/codeup/기초100제/Q1015.java
@@ -0,0 +1,11 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float f = sc.nextFloat();
+        System.out.printf("%.2f", f);
+    }
+}
