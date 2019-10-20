25ac8ba65e81e39ffbfc5c82915374669876abb0
moving-line <movinglinecheck@gmail.com>
2019-10-20 13:44:27 +0900

    Q1034 ~ Q1035

---
+++ src/main/java/codeup/기초100제/Q1034.java
@@ -0,0 +1,15 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d", sc.nextInt(8));
+
+//        String input = sc.nextLine();
+//
+//        int oct = Integer.valueOf(input, 8);
+//        System.out.printf("%d", oct);
+    }
+}
---
+++ src/main/java/codeup/기초100제/Q1035.java
@@ -0,0 +1,15 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1035 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%o", sc.nextInt(16));
+
+//        String input = sc.nextLine();
+//
+//        int hex = Integer.valueOf(input, 16);
+//        System.out.printf("%o", hex);
+    }
+}
