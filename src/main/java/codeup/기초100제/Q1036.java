d509f257805ef55e09c757c12ece24437941a616
moving-line <movinglinecheck@gmail.com>
2019-10-21 11:11:30 +0900

    Q1036 ~ Q1037

---
+++ src/main/java/codeup/기초100제/Q1036.java
@@ -0,0 +1,11 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1036 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println((int)input.charAt(0));
+    }
+}
