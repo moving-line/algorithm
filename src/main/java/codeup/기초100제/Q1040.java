4282b7426aee8f97ac48442d11487a607eec4c9d
moving-line <movinglinecheck@gmail.com>
2019-10-23 12:00:28 +0900

    Q1040 ~ Q1041

---
+++ src/main/java/codeup/기초100제/Q1040.java
@@ -0,0 +1,10 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1040 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(-sc.nextInt());
+    }
+}
---
+++ src/main/java/codeup/기초100제/Q1041.java
@@ -0,0 +1,11 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1041 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println((char)(input.charAt(0) + 1));
+    }
+}
