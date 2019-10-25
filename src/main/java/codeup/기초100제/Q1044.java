babb43f51c7ed5d5d7abcd905a521f8106617d14
moving-line <movinglinecheck@gmail.com>
2019-10-25 14:44:17 +0900

    Q1044 ~ Q1045

---
+++ src/main/java/codeup/기초100제/Q1044.java
@@ -0,0 +1,10 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1044 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLong() + 1);
+    }
+}
---
+++ src/main/java/codeup/기초100제/Q1045.java
@@ -0,0 +1,18 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1045 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.printf("%.2f", (double)a / b);
+    }
+}
