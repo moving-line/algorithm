9a6095c209f349d45e47c5da51b551baecdca0ea
moving-line <movinglinecheck@gmail.com>
2019-10-27 18:55:28 +0900

    Q1047 ~ Q1048

---
+++ src/main/java/codeup/기초100제/Q1047.java
@@ -0,0 +1,11 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(number << 1);
+    }
+}
