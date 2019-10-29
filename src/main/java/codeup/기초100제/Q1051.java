b99912dedf3e2c9e1275415a4b0ee342a70586f1
moving-line <movinglinecheck@gmail.com>
2019-10-29 10:39:08 +0900

    Q1051 ~ Q1052

---
+++ src/main/java/codeup/기초100제/Q1051.java
@@ -0,0 +1,13 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(b >= a ? 1 : 0);
+    }
+}
