1dd639d60a4febf2ebdf8ca2b0870d08b03b7f07
moving-line <movinglinecheck@gmail.com>
2019-11-07 11:50:17 +0900

    Q1069 ~ Q1070

---
+++ src/main/java/codeup/기초100제/Q1069.java
@@ -0,0 +1,27 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1069 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char input = sc.next().charAt(0);
+
+        switch (input) {
+            case 'A' :
+                System.out.println("best!!!");
+                break;
+            case 'B' :
+                System.out.println("good!!");
+                break;
+            case 'C' :
+                System.out.println("run!");
+                break;
+            case 'D' :
+                System.out.println("slowly~");
+                break;
+            default:
+                System.out.println("what?");
+        }
+    }
+}
