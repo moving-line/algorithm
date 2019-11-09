268214821ae75f5d8cb0e8ae4360c3ec48e5b723
moving-line <movinglinecheck@gmail.com>
2019-11-09 10:06:01 +0900

    Q1073

---
+++ src/main/java/codeup/기초100제/Q1073.java
@@ -0,0 +1,15 @@
+package codeup.기초100제;
+
+import java.util.Scanner;
+
+public class Q1073 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        while(input != 0) {
+            System.out.println(input);
+            input = sc.nextInt();
+        }
+    }
+}
