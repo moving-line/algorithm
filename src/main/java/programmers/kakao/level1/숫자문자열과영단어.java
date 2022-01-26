730fe0e58fc7998ed13ddfa839a78d9c19bcb35b
moving-line <movinglinecheck@gmail.com>
2022-01-26 18:23:36 +0900

    kakao 숫자 문자열과 영단어

---
+++ src/main/java/programmers/kakao/level1/숫자문자열과영단어.java
@@ -0,0 +1,24 @@
+package programmers.kakao.level1;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 숫자문자열과영단어 {
+    public int solution(String s) {
+        String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for (int i = 0; i < numbers.length; i++) {
+            s = s.replaceAll(numbers[i], String.valueOf(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+
+    @Test
+    public void test() {
+        assertThat(solution("one4seveneight")).isEqualTo(1478);
+        assertThat(solution("23four5six7")).isEqualTo(234567);
+        assertThat(solution("2three45sixseven")).isEqualTo(234567);
+        assertThat(solution("123")).isEqualTo(123);
+    }
+}
