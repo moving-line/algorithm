2706f74f6fcd04e1f7a4861b44e7574370b81584
moving-line <movinglinecheck@gmail.com>
2022-03-19 23:52:39 +0900

    Leetcode 162. Find Peak Element

---
+++ src/main/java/leetcode/L162_FindPeakElement.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class L162_FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+        return nums.length - 1;
+    }
+}
